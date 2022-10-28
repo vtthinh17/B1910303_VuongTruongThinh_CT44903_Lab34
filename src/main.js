4f2b7e8f542114a3d745b0e80e6bd9520586db86
vtthinh <vtthinh17st@gmail.com>
2022-10-28 15:05:14 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-createApp(App).mount('#app')
+
+import './assets/main.css'
+import router from "./router";
+createApp(App).use(router).mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,12 @@ export default defineConfig({
   },
   server: {
     port: 3001,
+    proxy: {
+      "/api": {
+      target: "http://localhost:3000/",
+      changeOrigin: true,
+      },
+    }
   },
     
 })
